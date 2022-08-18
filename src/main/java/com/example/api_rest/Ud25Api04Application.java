e294edc06abb01919edcd6bd87130b3853aa470d
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-18 09:29:44 +0200

    Proyecto vacio

---
+++ src/main/java/com/example/api_rest/Ud25Api04Application.java
@@ -0,0 +1,13 @@
+package com.example.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud25Api04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud25Api04Application.class, args);
+	}
+
+}
